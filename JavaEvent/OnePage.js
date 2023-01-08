0c7d274589f6ec5865aad4fc0d742361c1b124d7
YCT2000 <yimchingtung@gmail.com>
2023-01-08 16:58:14 +0800

    updated

--- JavaEvent/OnePage.js
+++ JavaEvent/OnePage.js
@@ -63,3 +63,86 @@ $('.slider').slick({
   ]
 });
 }
+
+<script>
+/*TV*/
+const TV = document.getElementsByClassName('TV') 
+let options2={
+    rootMargin:'-10%',
+    threshold:0.6
+}
+
+const observer2 = new IntersectionObserver(entries => {
+entries.forEach(entry =>{
+if(entry.isIntersecting){
+    entry.target.classList.add('activeTV')
+    }
+
+else{
+    console.log('no')
+    entry.target.classList.remove('activeTV')
+    }
+
+})
+})
+
+for(i =0; i<TV.length;i++){
+observer2.observe(TV[i])
+console.log('hey')
+}
+
+const Tiger = document.getElementsByClassName('tiger') 
+let options3={
+    rootMargin:'-10%',
+    threshold:0.6
+}
+
+
+/*Tiger*/
+const observer3 = new IntersectionObserver(entries => {
+entries.forEach(entry =>{
+if(entry.isIntersecting){
+    entry.target.classList.add('activeTiger')
+    }
+
+else{
+    console.log('no')
+    entry.target.classList.remove('activeTiger')
+    }
+
+})
+})
+
+for(i =0; i<Tiger.length;i++){
+observer3.observe(Tiger[i])
+console.log('hey')
+}
+
+/*Watch*/
+const Watch = document.getElementsByClassName('watch') 
+let options4={
+    rootMargin:'-10%',
+    threshold:0.6
+}
+
+const observer4 = new IntersectionObserver(entries => {
+entries.forEach(entry =>{
+if(entry.isIntersecting){
+    console.log('watch is here')
+    entry.target.classList.add('activeWatch')
+    }
+
+else{
+    console.log('no')
+    entry.target.classList.remove('activeWatch')
+    }
+
+})
+})
+
+for(i =0; i<Watch.length;i++){
+observer4.observe(Watch[i])
+console.log('hey')
+}
+
+</script>
